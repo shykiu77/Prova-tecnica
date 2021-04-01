4341c7acd9a9f23ebf9043311058b1959b66797d
Carlos Gabriel Santos de Melo <carlosgabrielsns@gmail.com>
2021-04-01 14:27:39 -0300

    Implementadas funções de comunicação com o backend.

--- frontend/src/app/components/clientes/clientes.service.ts
+++ frontend/src/app/components/clientes/clientes.service.ts
@@ -1,9 +1,38 @@
+import { Cliente } from './clientes.model';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesService {
 
-  constructor() { }
+  baseUrl =  "http://localhost:3001/clientes"
+
+  constructor(private http: HttpClient) { }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.baseUrl,cliente)
+  }
+
+  read() : Observable<Cliente[]> {
+    return this.http.get<Cliente[]>(this.baseUrl)
+  }
+
+  readById(id: string): Observable<Cliente> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.get<Cliente>(url)
+  }
+
+  update(cliente: Cliente): Observable<Cliente> {
+    const url = `${this.baseUrl}/${cliente.id}`
+    return this.http.put<Cliente>(url,cliente)
+  }
+
+  delete(id: string): Observable<Cliente> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.delete<Cliente>(url)
+  }
+
 }
